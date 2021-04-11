f18ac26d7692836e4d700fb1ecf0e7b4eb6352b3
akhilgop <akh_il@hotmail.com>
2021-04-11 17:16:07 +0530

    Bug Fixes
    Responsive bug fixes

--- js/slider.js
+++ js/slider.js
@@ -3,11 +3,11 @@ window.onload = function() {
         infinite: false,
         speed: 300,
         slidesToShow: 4,
-        slidesToScroll: 1,
+        slidesToScroll: 3,
         //swipeToSlide: true,
         arrows: true,
         centerPadding: 0,
-        //variableWidth: true,
+        variableWidth: true,
         responsive: [{
                 breakpoint: 992,
                 settings: {
@@ -47,3 +47,43 @@ window.onload = function() {
         preventDefault: false,
     });
 }
+
+
+
+
+
+
+
+
+// ------------------------------------------------------Slider
+$('.variable-width').slick({
+    centerMode: true,
+    infinite: true,
+    //centerPadding: '60px',
+    slidesToShow: 1,
+    speed: 300,
+    variableWidth: true,
+    //draggable: true,
+});
+
+$('.variable-width').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+    //console.log('beforeChange', currentSlide, nextSlide);
+    // $(".slick-current").removeClass("high-light");
+    // $(".slick-current").addClass("normal");
+    //console.log($( ".slick-slide[data-slick-index='"+nextSlide+"']" ));
+    var nextSlick = $(".slick-slide[data-slick-index='" + nextSlide + "']");
+    // nextSlick.addClass("high-light");
+    // nextSlick.removeClass("normal");
+});
+
+$('.variable-width').on('afterChange', function(event, slick, currentSlide) {
+    //console.log('afterChange', currentSlide);
+    //$(".slick-current").addClass("high-light");
+    //$(".slick-current").removeClass("normal");
+});
+setTimeout(() => {
+    //console.log($(".slick-current"));
+    // $(".slick-current").removeClass("normal");
+    // $(".slick-current").addClass("high-light");
+    //$('.variable-width').slick('setPosition');
+}, 100);
