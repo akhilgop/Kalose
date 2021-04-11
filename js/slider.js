d1a9bcf130397eea6fa6b4e43c0452adf6520bf6
akhilgop <akh_il@hotmail.com>
2021-04-11 15:36:45 +0530

    Slider Added
    Slider for Vitamins tablets added

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,49 @@
-$('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    fade: true,
-    asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: false,
-    centerMode: true,
-    focusOnSelect: true,
-    initialSlide: 2,
-    arrows: true
-});
+window.onload = function() {
+    $('.slide').slick({
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        //swipeToSlide: true,
+        arrows: true,
+        centerPadding: 0,
+        variableWidth: true,
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    centerMode: false,
+                    centerPadding: '10px',
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    centerMode: false,
+                    centerPadding: '30px',
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerPadding: '0px',
+                    slidesToShow: 1,
+                    centerMode: false,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
+    var scroll = new IScroll('.slick-list.draggable', {
+        scrollX: true,
+        scrollbars: true,
+        interactiveScrollbars: true,
+        fadeScrollbars: true,
+        hideScrollbars: true,
+        scrollY: false,
+        eventPassthrough: true,
+        preventDefault: false,
+    });
+}
